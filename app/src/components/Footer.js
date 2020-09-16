9e882ffd3da1a17ef7737506ab706f277c00f696
William Fullerton <wrk.fullerton@gmail.com>
2020-09-16 10:52:59 -0500

    first commit

---
+++ app/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            
+            <h4> © 2020. Will Fullerton - All Rights Reserved.</h4>
+            <h5>Made with React</h5>
+        </div>
+    )
+}
+
+export default Footer

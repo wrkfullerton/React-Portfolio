ef1987ce53757bab05f77533e7348c9ecdaa1527
William Fullerton <wrk.fullerton@gmail.com>
2020-09-16 10:56:07 -0500

    changes made to file structure = added code from first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>Will Fullerton</h2>
+            <h4>Portfolio</h4>
+
+        </div>
+    )
+}
+
+export default Header
